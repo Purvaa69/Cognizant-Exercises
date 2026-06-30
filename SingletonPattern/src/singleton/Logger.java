cb703c1f8cad629d924d0845636ba10e194bc7c3
Purvaa69 <166300460+Purvaa69@users.noreply.github.com>
2026-06-30 16:32:56 +0530

    Added Singleton Pattern Exercise
    Completed Exercise 1 - Singleton Design Pattern

---
+++ SingletonPattern/src/singleton/Logger.java
@@ -0,0 +1,26 @@
+package singleton;
+
+public class Logger {
+
+    // Create only one object
+    private static Logger instance;
+
+    // Private constructor
+    private Logger() {
+        System.out.println("Logger Created");
+    }
+
+    // Public method to get the object
+    public static Logger getInstance() {
+
+        if (instance == null) {
+            instance = new Logger();
+        }
+
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println("LOG: " + message);
+    }
+}
