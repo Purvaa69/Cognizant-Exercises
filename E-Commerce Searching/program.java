aa76ed40fd2448c954a1a72c65ea4ab29a8d3c08
Purvaa69 <166300460+Purvaa69@users.noreply.github.com>
2026-07-02 13:29:52 +0530

    Added E-Commerce Searching Exercise-2

---
+++ E-Commerce Searching/program.java
@@ -0,0 +1,29 @@
+public class program {
+
+    public static void main(String[] args) {
+
+        Product[] products = {
+
+                new Product(101, "Laptop", "Electronics"),
+                new Product(102, "Phone", "Electronics"),
+                new Product(103, "Shoes", "Fashion"),
+                new Product(104, "Watch", "Accessories"),
+                new Product(105, "Bag", "Fashion")
+        };
+
+        Product result1 = LinearSearch.linearSearch(products, 104);
+
+        if(result1 != null){
+
+            System.out.println("Linear Search Found : " + result1.productName);
+        }
+
+        Product result2 = BinarySearch.binarySearch(products,105);
+
+        if(result2 != null){
+
+            System.out.println("Binary Search Found : " + result2.productName);
+        }
+
+    }
+}
